4ca650898fa4a019ec84d3a183c2bbf448fca02e
Anas Mufti <muftianas97@gmail.com>
2025-04-21 19:50:41 +0500

    initial commit

--- Downloads/pythonProgramming/statistics_1.py
+++ Downloads/pythonProgramming/statistics_1.py
@@ -2,11 +2,13 @@
 import pandas as pd 
 import numpy as np
 
-df = pd.read_csv("/Users/apple/Downloads/pythonProgramming/stats.csv")
+df = pd.read_csv("/Users/apple/Downloads/pythonProgramming/HeightWeight.csv")
+print(df["Height(Inches)"])
+print(df["Height(Inches)"].describe())
+# print(df["Monthly Income"].quantile(.5))
 
-print(df)
-print(df.describe())
-print(df["Monthly Income"].quantile(.5))
+
+
 
 
 
